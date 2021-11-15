bca6f4ca850a41fac3575d62347649f1ab75eff9
COPA <carpeas@outlook.es>
2021-11-15 11:13:42 +0100

    Inicio

---
+++ src/proyecto_futuro/Proyecto_Futuro.java
@@ -0,0 +1,26 @@
+
+package proyecto_futuro;
+
+
+public class Proyecto_Futuro {
+
+    public static void main(String[] args) {
+        System.out.println ("Estudiante: " + args [0]);
+        System.out.println ("Módulos matriculados: " + args [1]);
+        System.out.println ("Horas de estudio dedicadas: " + args [2]);
+        
+        System.out.println(Proyecto_Futuro.veredicto(args[1],args[2]));
+    }
+    
+   public static String veredicto(String valor1, String valor2) {
+    if (valor1.equals(valor2)) {
+        return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+    if (Integer.valueOf(valor1) > Integer.valueOf(valor2)){
+        return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+    }
+    return "Ideal. Trabajas los contenidos en casa.";
+    }
+}
+
+
